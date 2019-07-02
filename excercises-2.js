fc24131afc7a67182f1c939ce9edb254701d4f1c
Geoff Hutson <geoff.hutson.89@gmail.com>
2019-07-02 16:24:07 -0700

    finished questions

--- excercises-2.js
+++ excercises-2.js
@@ -4,6 +4,14 @@
 // if-then-else construct available in JavaScript.
 // ---------------------
 
+function max(a,b) {
+    if (a > b) {
+        return a;
+    } else{
+        return b;
+    }
+}
+
 console.assert(max(2,3) === 3)
 console.assert(max(23,24) === 24)
 console.assert(max(-23, 12) === 12)
@@ -12,6 +20,16 @@ console.assert(max(-23, 12) === 12)
 // Define a function maxOfThree() that takes three 
 // numbers as arguments and returns the largest of them.
 // ---------------------
+
+function maxOfThree(a,b,c) {
+    if (a > b || a > c){
+        return a;
+    } else if (b > a || b > c ){
+        return b;
+    }else {
+        return c;
+    }
+}
 
 console.assert(maxOfThree(2,56,3) === 56)
 console.assert(maxOfThree(12,3,4) === 12)
@@ -23,12 +41,16 @@ console.assert(maxOfThree(-12,4,-5) === 4)
 // vowel, false otherwise.
 // ---------------------
 
+function isVowel(char) {
+    return (/^[aeiou]$/i).test(char);
+}
+
 console.assert(isVowel(0) === false);
 console.assert(isVowel("B") === false);
 console.assert(isVowel("b") === false);
 console.assert(isVowel("a") === true);
 console.assert(isVowel("E") === true);
-console.assert(isVowel("2") === false)
+console.assert(isVowel("2") === false);
 
 // ---------------------
 // Write a function rovarspraket() that will translate 
@@ -37,6 +59,34 @@ console.assert(isVowel("2") === false)
 // For example, translate("this is fun") should return 
 // the string "tothohisos isos fofunon".
 // ---------------------
+
+function rovarspraket(str){
+    
+    var j = '';
+
+    // for (i = 0; i < str.length; i++) {
+    //     if (str.charAt(i) == "a" || str.charAt(i) == "e" || str.charAt(i) == "i" || str.charAt(i) == "o" || str.charAt(i) == "u" || str.charAt(i) != 0){
+    //         j += str[i];
+    //     } else {
+    //      j += str[i] + 'o' + str[i];
+    //     }
+    //  return j;
+    // }
+    
+
+    if(typeof str !== 'string'){
+        return str + ''
+    }
+    for (i = 0; i < str.length; i++) {
+        if(isVowel(str[i])){
+            j += str[i]
+        }else{
+            j += str[i] + "o" + str[i];
+        }
+    }
+    return j
+}
+
 
 console.assert(rovarspraket("a") === "a")
 console.assert(rovarspraket("b") === "bob")
@@ -51,6 +101,12 @@ console.assert(rovarspraket(0) === "0")
 // return the string "ratset gaj".
 // ---------------------
 
+function reverse(str) {
+
+    var newStr = str.split("").reverse().join("");
+
+    return newStr;
+}
 
 console.assert(reverse("books") === "skoob")
 console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
